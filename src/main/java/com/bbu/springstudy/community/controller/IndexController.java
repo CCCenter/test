3381a73fcd6fb1da8fe8a6ab2542ae39ffa77323
CCCenter <1021012307@qq.com>
2019-09-22 22:46:00 +0800

    add index

--- src/main/java/com/bbu/springstudy/community/controller/IndexController.java
+++ src/main/java/com/bbu/springstudy/community/controller/IndexController.java
@@ -1,4 +1,11 @@
 package com.bbu.springstudy.community.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+    @GetMapping("/")
+    //什么都不输入默认返回index
+    public String index(){ return "index";}
 }
