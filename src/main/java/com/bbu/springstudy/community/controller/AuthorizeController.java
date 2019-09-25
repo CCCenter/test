fff0db21bd4b46306641ca27ba549e9a3511e8db
CCCenter <1021012307@qq.com>
2019-09-25 11:14:57 +0800

    add h2db

--- src/main/java/com/bbu/springstudy/community/controller/AuthorizeController.java
+++ src/main/java/com/bbu/springstudy/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.bbu.springstudy.community.controller;
 
 import com.bbu.springstudy.community.dto.AccessTokenDTO;
 import com.bbu.springstudy.community.dto.GithubUser;
+import com.bbu.springstudy.community.mapper.UserMapper;
+import com.bbu.springstudy.community.model.User;
 import com.bbu.springstudy.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -17,16 +22,18 @@ public class AuthorizeController {
 
     @Value("${github.client.id}") //去配置文件读取 当spring容器启动的时候 读取配置文件 把key - value 存到一个map 里面  使用Value注解获取
     private String clientId;
-
     @Value("${github.client.secret}")
     private String clientSecret;
-
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){//spring 自动把上下文的request 放入
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -35,9 +42,31 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            //登陆成功写 cookie和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
 
-        return "index";
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", user);
+            return "redirect:/"; //重定向
+
+        }else{
+            //登陆失败 重新登陆
+            return "redirect:/"; //重定向
+        }
+    }
+
+    public UserMapper getUserMapper() {
+        return userMapper;
+    }
+
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
     }
 }
---
+++ src/main/java/com/bbu/springstudy/community/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.bbu.springstudy.community.mapper;
+
+import com.bbu.springstudy.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
